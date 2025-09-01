b0aeaf9d76227dc3c2561084484b761a0ffd7736
VARSHAESC <varsha.dileepkumari@esc-ind.com>
2025-09-01 11:31:49 +0530

    first commit

---
+++ appAPI.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "511553e9-8b4b-43b6-a7ff-dfe0ee38e7ca",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
